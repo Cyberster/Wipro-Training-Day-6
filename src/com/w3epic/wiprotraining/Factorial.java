e94873613843c63fa9f63c7e28b6e07ca7939389
Dishant Rathi <dishantrathi@users.noreply.github.com>
2019-03-05 15:48:04 +0530

    Update Factorial.java

--- src/com/w3epic/wiprotraining/Factorial.java
+++ src/com/w3epic/wiprotraining/Factorial.java
@@ -1,5 +1,19 @@
 package com.w3epic.wiprotraining;
 
-public class Factorial {
-	
+import java.io.*;
+import  java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+
+	public int nFactorial(int input1){
+		// Read only region end
+		int n = input1;
+		if (n == 0)    
+            return 1;    
+		else    
+            return(n * nFactorial(input1-1));        
+		
+	}
 }
