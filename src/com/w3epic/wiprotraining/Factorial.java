bac1f633f8086c4929a10d81a84d84a22bebbe05
Arpan Das <arp14@yahoo.com>
2019-04-13 14:14:54 +0530

    Update Factorial.java

--- src/com/w3epic/wiprotraining/Factorial.java
+++ src/com/w3epic/wiprotraining/Factorial.java
@@ -1,5 +1,18 @@
 package com.w3epic.wiprotraining;
 
+import java.util.Scanner;
+
 public class Factorial {
+	private static int factorial(int num) {
+	    if (num < 2) return 1;
+	    return num * factorial(num - 1);
+	}
 	
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter a number: ");
+	    int num = sc.nextInt();
+	    System.out.println("\n" + num + "! = " + factorial(num));
+	    sc.close();
+	}
 }
